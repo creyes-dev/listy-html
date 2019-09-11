11cf0a7b1528f1f792c500cb6192717d25138250
creyes-dev <creyes.dev@gmail.com>
2019-09-11 20:00:17 -0300

    animación suave de desplazamiento a las secciones de la pagina

--- js/main.js
+++ js/main.js
@@ -12,10 +12,47 @@
     }
   }
 
+  const onNavItemClick = () => {
+    const navItemList = document.querySelectorAll(".ly-section-link");
+    const navItems = [...navItemList];
+
+    navItems.forEach(item => {
+      item.addEventListener("click", event => {
+        event.preventDefault();
+
+        const sectionId = event.target.getAttribute("href") || event.target.dataset.href;
+        scrollTopSection(sectionId);
+      });
+    });
+  }
+
+  const scrollTopSection = sectionId => {
+
+    let sectionPosition, sectionOffset;
+    const navigationHeight = document.querySelector("header nav").offsetHeight;
+    const pageWidth = window.innerWidth;
+
+    if(sectionId !== "#") {
+      sectionOffset = document.querySelector(sectionId).offsetTop;
+      sectionPosition = pageWidth > mobileWidth ? sectionOffset - navigationHeight : sectionOffset;
+    } else {
+      sectionPosition = 0;
+    }
+
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': sectionPosition
+    });
+
+  }
+
   // The scroll event allows to react on a page or element scrolling. There are quite a few good things we can do here.
   // window.addEventListener('scroll', function() { addMenuBackground });
   window.addEventListener("scroll", () => {
     addMenuBackground();
   }); 
+
+  onNavItemClick();
 
 })();
