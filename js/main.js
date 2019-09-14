e520b13a20fb43e21329c9d885330a2b2e4106e1
creyes-dev <creyes.dev@gmail.com>
2019-09-14 16:39:05 -0300

    adaptar la barra de nagegación a dispositivos moviles

--- js/main.js
+++ js/main.js
@@ -12,6 +12,29 @@
     }
   }
 
+	const reorderResponsiveMenu = () => {
+    const pageWidth = window.innerWidth;
+    const navContainer = document.querySelector("header nav .ly-container");
+    const navigation = document.querySelector("header nav .ly-navigation");
+    const mobileNavigation = document.querySelector("body > .ly-navigation");
+
+    if (pageWidth <= mobileWidth && navigation) {
+      document.body.insertAdjacentElement("afterbegin", navigation);
+    } else if (pageWidth > mobileWidth && mobileNavigation) {
+      navContainer.insertAdjacentElement("beforeend", mobileNavigation);
+    }
+  }
+
+  const mobileMenuToggle = () => {
+    const menuToggle = document.querySelector(".ly-nav-toggle");
+
+
+    menuToggle.addEventListener("click", () => {
+      const mobileNavigation = document.querySelector("body > .ly-navigation");
+      mobileNavigation.classList.toggle("ly-navigation-opened");
+    })
+  }
+
   const onNavItemClick = () => {
     const navItemList = document.querySelectorAll(".ly-section-link");
     const navItems = [...navItemList];
@@ -107,6 +130,12 @@
     addMenuBackground();
   })
 
+  window.addEventListener("resize", () => {
+    reorderResponsiveMenu();
+  })
+
+  reorderResponsiveMenu();
+  mobileMenuToggle();
   onNavItemClick();
   onTestimonialChange();
   onGalleryImageClick();
